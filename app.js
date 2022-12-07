c11b66aa971cc7c48ef9a7fcd9d8ac14463b693c
unartcs <unartcs@gmail.com>
2022-12-07 18:21:48 +0100

    Edited app.js index.html and style.css
    Changed the data that is being displayed on the page.
    Made a background image generator related to the searched location.
    Worked on style a bit, still need to make it look better!

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const API_KEY = 'f9df2d7c6e89631c71bb7de05d14b3c8';
+const BG_API_KEY = 'kPHO5hzzhKaD7uugePlIp_1yh4BmLCFEjBxTXyABX2M'
 const searchBar = document.querySelector('.search_bar')
 const searchBarButton = document.querySelector('.search_container > button')
 const displayWeatherContainer = document.querySelector('.display_weather_container')
 const errorContainer = document.querySelector('.error-container')
 let tempFormat = 'celsius'
 const url = 'https://api.openweathermap.org/data/2.5/weather?q='
+const bgURL = 'https://api.unsplash.com/photos/random/?client_id='
 
 function fixCases(string) {
     return string.split(' ').map((item, index) => item.replace(item.slice(0, 1), item.slice(0, 1).toUpperCase())).join(' ')
@@ -15,9 +17,35 @@ async function getData(location) {
         const response = await fetch(`${url}${location}&appid=${API_KEY}`, { mode: "cors" })
         const data = await response.json();
         (response.ok) ? displayWeather(data) : handleError(response.statusText)
+        console.log(data)
     } catch (error) {
         console.log(error)
     }
+}
+
+
+async function getPictureData(string) {
+    try {
+        const response = await fetch(`${bgURL}${BG_API_KEY}&query=${string}`, { mode: "cors" })
+        const data = await response.json();
+        if (response.ok) {
+            console.log(response)
+            console.log(data)
+            changeBackgroundImage(data.urls.regular)
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+function changeBackgroundImage(link) {
+    const bodySelector = document.querySelector('body')
+    bodySelector.style.backgroundImage = `url(${link})`
+    bodySelector.style.backgroundRepeat  = 'no-repeat'
+    bodySelector.style.backgroundPosition = "center";
+    bodySelector.style.backgroundSize = 'cover'
+
 }
 
 function handleError(response) {
@@ -34,19 +62,21 @@ function displayWeather(data) {
     const weatherContainer = document.querySelector('.display_weather'),
         locationDiv = weatherContainer.querySelector('.display_location'),
         descDiv = weatherContainer.querySelector('.weather_description'),
-        tempDiv = weatherContainer.querySelector('.weather_temp'),
-        maxTempDiv = weatherContainer.querySelector('.temp_max'),
-        minTempDiv = weatherContainer.querySelector('.temp_min'),
+        // tempDiv = weatherContainer.querySelector('.weather_temp'),
+        // maxTempDiv = weatherContainer.querySelector('.temp_max'),
+        // minTempDiv = weatherContainer.querySelector('.temp_min'),
         feelsLikeDiv = weatherContainer.querySelector('.feels_like'),
         humidityDiv = weatherContainer.querySelector('.humidity'),
-        minTempSpan = document.createElement('span');
+        windsDiv = weatherContainer.querySelector('.wind_speed');
     locationDiv.textContent = data.name;
-    descDiv.textContent = fixCases(data.weather[0].description)
-    tempDiv.textContent = 'Temperature: ' + calcTemp(data.main.temp)
-    maxTempDiv.textContent = 'Max Temperature: ' + calcTemp(data.main.temp_max)
-    minTempDiv.textContent = 'Min Temperature: ' + calcTemp(data.main.temp_min)
+    // tempDiv.textContent =
+    descDiv.textContent = fixCases(data.weather[0].description) + ' ' + calcTemp(data.main.temp)
+    // maxTempDiv.textContent = 'Max Temperature: ' + calcTemp(data.main.temp_max)
+    // minTempDiv.textContent = 'Min Temperature: ' + calcTemp(data.main.temp_min)
     feelsLikeDiv.textContent = 'Feels Like: ' + calcTemp(data.main.feels_like)
     humidityDiv.textContent = 'Humidity: ' + data.main.humidity + '%'
+    windsDiv.textContent = 'Wind: ' + data.wind.speed + 'm/s'
+    getPictureData(`${data.name}`)
 }
 
 function calcTemp(temp) {
